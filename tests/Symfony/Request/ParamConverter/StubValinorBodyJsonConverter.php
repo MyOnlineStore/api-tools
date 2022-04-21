054fae2369483de3d124f816f3fa5fa9f05a721b
Frank Verhoeven <frankverhoeven@me.com>
2022-04-21 11:03:21 +0200

    security: Bump `cuyz/valinor` to `^0.7.0` (#6)

--- tests/Symfony/Request/ParamConverter/StubValinorBodyJsonConverter.php
+++ tests/Symfony/Request/ParamConverter/StubValinorBodyJsonConverter.php
@@ -24,6 +24,6 @@ final class StubValinorBodyJsonConverter extends ValinorParamConverter
     protected function getMapperBuilder(): MapperBuilder
     {
         return parent::getMapperBuilder()
-            ->bind(static fn (string $id): StubId => StubId::fromString($id));
+            ->registerConstructor([StubId::class, 'fromString']);
     }
 }
